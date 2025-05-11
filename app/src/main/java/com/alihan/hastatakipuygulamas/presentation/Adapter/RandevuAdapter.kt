ae8b5f42b78edc4e4b69a5d9779c5e53f334d294
AlihanCelik <alihan_08032003@hotmail.com>
2025-05-11 16:05:09 +0300

    .

---
+++ app/src/main/java/com/alihan/hastatakipuygulamas/presentation/Adapter/RandevuAdapter.kt
@@ -0,0 +1,42 @@
+package com.alihan.hastatakipuygulamas.presentation.Adapter
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.recyclerview.widget.RecyclerView
+import com.alihan.hastatakipuygulamas.R
+import com.alihan.hastatakipuygulamas.data.model.Randevu
+import com.alihan.hastatakipuygulamas.databinding.ItemRandevuBinding
+
+class RandevuAdapter : RecyclerView.Adapter<RandevuAdapter.RandevuViewHolder>() {
+
+    private var randevuList: List<Randevu> = emptyList()
+
+    fun setData(newList: List<Randevu>) {
+        this.randevuList = newList
+        notifyDataSetChanged()
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int):RandevuViewHolder {
+        val inflater = LayoutInflater.from(parent.context)
+        val view = DataBindingUtil.inflate<ItemRandevuBinding>(
+            inflater, R.layout.item_randevu, parent, false
+        )
+        return RandevuViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: RandevuViewHolder, position: Int) {
+        val randevu = randevuList[position]
+        holder.view.randevu = randevu
+        holder.view.itemLy.setOnClickListener {
+
+
+        }
+    }
+
+    override fun getItemCount(): Int {
+        return randevuList.size
+    }
+
+    class RandevuViewHolder(var view: ItemRandevuBinding) : RecyclerView.ViewHolder(view.root)
+}
